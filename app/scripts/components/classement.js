243f67f35c9998280858fa6a3ad101fed49fe27d
JulienUsson <usson.julien@gmail.com>
2016-10-08 18:50:54 +0200

    classement

--- app/scripts/components/classement.js
+++ app/scripts/components/classement.js
@@ -16,15 +16,15 @@ function classementController() {
   vm.users = [];
   for (var i = 0; i < 200; i++) {
     if(i == 8) {
-     this.users.push({name: "Mathilde Dupont", index: i, score: 1000 - i});
+     this.users.push({name: "Mathilde Dupont", index: i, score: 1000 - i*33});
     } else {
-     this.users.push({name: "John Do", index: i, score: 1000 - i});
+     this.users.push({name: "John Do", index: i, score: 1000 - i*33});
     }
   }
 
 
-  if(vm.actualUser > 3) {
-    vm.topIndex = vm.actualUser-3;
+  if(vm.actualUser > 2) {
+    vm.topIndex = vm.actualUser-2;
   } else {
     vm.topIndex = vm.actualUser;
   }
