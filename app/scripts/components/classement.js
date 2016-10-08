36db3e83abf72a6e137f09e07798f8a81cc1a59b
JulienUsson <usson.julien@gmail.com>
2016-10-08 18:00:17 +0200

    classement

--- app/scripts/components/classement.js
+++ app/scripts/components/classement.js
@@ -10,4 +10,23 @@ module('hackathonApp').
 
 function classementController() {
   var vm = this;
+
+  vm.actualUser = 8;
+
+  vm.users = [];
+  for (var i = 0; i < 200; i++) {
+    if(i == 8) {
+     this.users.push({name: "Mathilde Dupont", index: i, score: 1000 - i});
+    } else {
+     this.users.push({name: "John Do", index: i, score: 1000 - i});
+    }
+  }
+
+
+  if(vm.actualUser > 3) {
+    vm.topIndex = vm.actualUser-3;
+  } else {
+    vm.topIndex = vm.actualUser;
+  }
+
 }
