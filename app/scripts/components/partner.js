84a12c851a70e56528e5110bd37bfc3e0b41adba
JulienUsson <usson.julien@gmail.com>
2016-10-09 09:34:14 +0200

    partner

--- app/scripts/components/partner.js
+++ app/scripts/components/partner.js
@@ -11,4 +11,10 @@ module('hackathonApp').
 function partnerController() {
   var vm = this;
 
+  vm.partners=[
+    {'title': 'EUM', 'subTitle': 'École Universitaire de Management', 'imageSrc': 'images/components/partner/EUM.png'},
+    {'title': 'ISIMA', 'subTitle': 'Institut Supérieur d\'Informatique, de Modélisation et de leurs Applications', 'imageSrc': 'images/components/partner/ISIMA.png'},
+    {'title': 'SMTC', 'subTitle': '', 'imageSrc': 'images/components/partner/SMTC.png'},
+    {'title': 'Ville de Clermont-Ferrand', 'subTitle': '', 'imageSrc': 'images/components/partner/Ville_Clermont.jpg'}
+  ];
 }
